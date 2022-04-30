0d439753dcc03ccfeadb6d999a1be0d6f46479f0
tengchunlei <5702225+tengchunlei@user.noreply.gitee.com>
2022-04-30 14:50:33 +0800

    配置 CSS

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
