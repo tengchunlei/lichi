b150e12130feb16e867d0048025dec1027fc58d6
tengchunlei <5702225+tengchunlei@user.noreply.gitee.com>
2022-05-03 07:33:40 +0800

    add Remark

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,6 +4,7 @@ import {Link, NavLink} from "react-router-dom";
 import Icon from "./Icon";
 
 const NavWrapper=styled.div`
+  background: #ffffff;
 line-height: 24px;
   box-shadow:0 0 3px rgba(0,0,0,0.25) ;
 > ul{
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -30,7 +30,25 @@ const TagSection= styled.section`
   
 `
 const NoteSection= styled.section`
-
+background:#f5f5f5;
+  padding:10px 16px;
+  font-size: 14px;
+  
+  > label{
+    display:flex;
+    align-items: center;
+    > span {margin-right:16px;
+    white-space: nowrap;
+    }
+    > input{
+      display:block;
+      width:100%;
+      height:72px;
+      background:none;
+      border:none;
+    }
+    
+  }
 `
 
 const CategorySection= styled.section`
@@ -57,7 +75,7 @@ function Money() {
             <NoteSection>
                 <label>
                     <span>备注</span>
-                    <input type="text"/>
+                    <input type="text" placeholder="添加备注"/>
 
                 </label>
             </NoteSection>
