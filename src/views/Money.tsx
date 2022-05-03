9e32debd7007409a5a1c4a7557117c0f0bf2128f
tengchunlei <5702225+tengchunlei@user.noreply.gitee.com>
2022-05-03 16:38:29 +0800

    complete Money.tsx type UI

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -52,7 +52,29 @@ background:#f5f5f5;
 `
 
 const CategorySection= styled.section`
-
+   font-size: 24px;
+   > ul{
+     background:#c4c4c4 ;
+     display: flex;
+     > li{
+       width: 50%;
+       text-align: center;
+       padding:16px 0;
+       position: relative;
+       &.selected::after{
+         content:'';
+         display: block;
+         height:3px;
+         background: #333;
+         position:absolute;
+         bottom:0;
+         width:100%;
+         left:0;
+         
+       }
+     }
+     
+   }
 `
 
 const NumberPadSection= styled.section`
@@ -82,7 +104,7 @@ function Money() {
 
            <CategorySection>
                <ul>
-                   <li>支出</li>
+                   <li className="selected">支出</li>
                    <li>收入</li>
                </ul>
            </CategorySection>
