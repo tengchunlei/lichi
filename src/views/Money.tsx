ae307e93c2470df3e80838ef7fc4468e35843348
tengchunlei <5702225+tengchunlei@user.noreply.gitee.com>
2022-05-04 10:25:30 +0800

    complete Money.tsx NumberPAD UI

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -78,7 +78,82 @@ const CategorySection= styled.section`
 `
 
 const NumberPadSection= styled.section`
-    
+   display:flex;
+   flex-direction: column;
+  > .output{
+    background: white;
+    font-size: 36px;
+    line-height:72px ;
+    text-align: right;
+    padding: 0 16px;
+    box-shadow:inset 0 -5px 5px -5px rgba(0,0,0,0.25)
+    ,inset 0 5px 5px -5px rgba(0,0,0,0.25);
+
+
+  }
+    > .pad{
+      > button{
+        font-size:18px;
+        float:left;
+        width: 25%;
+        height:64px;
+        border:none;
+        &.ok{
+          height:128px;
+          float:right;
+        }
+        &.dot{
+          width: 25%;
+        }
+        &.zero{
+          width: 50%;
+        }
+        
+        &:nth-child(1){
+          background: #f2f2f2;
+        }
+
+        &:nth-child(2),
+        &:nth-child(5){
+          background: #E0E0E0;
+        }
+
+        &:nth-child(3),
+        &:nth-child(6),
+        &:nth-child(9){
+        background:#D3D3D3;
+      }
+
+        &:nth-child(4),
+        &:nth-child(7),
+        &:nth-child(10){
+        background: #C1C1C1;
+      }
+
+        &:nth-child(8),
+        &:nth-child(11),
+        &:nth-child(13){
+        background:#B8B8B8;
+      }
+
+        &:nth-child(12){
+        background:#9A9A9A ;
+      }
+
+        &:nth-child(14){
+        background: ;#A9A9A9
+      }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+      }
+    }
 `
 
 
@@ -110,8 +185,10 @@ function Money() {
            </CategorySection>
 
            <NumberPadSection>
-               <div></div>
-               <div>
+               <div className="output">
+               10000000
+               </div>
+               <div className="pad clearfix">
                    <button>1</button>
                    <button>2</button>
                    <button>3</button>
@@ -123,9 +200,9 @@ function Money() {
                    <button>7</button>
                    <button>8</button>
                    <button>9</button>
-                   <button>OK</button>
-                   <button>0</button>
-                   <button>.</button>
+                   <button className="ok">OK</button>
+                   <button className="zero">0</button>
+                   <button className="dot">.</button>
                </div>
            </NumberPadSection>
 
