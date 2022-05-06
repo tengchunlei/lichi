e15e99d8ae1947b0894987dcf46687083e6c9a90
tengchunlei <5702225+tengchunlei@user.noreply.gitee.com>
2022-05-06 12:37:26 +0800

    complete category switch

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -16,12 +16,7 @@ function Money() {
         <MyLayout>
             <TagsSection/>
             <NoteSection/>
-            <CategorySection>
-               <ul>
-                   <li className="selected">支出</li>
-                   <li>收入</li>
-               </ul>
-           </CategorySection>
+            <CategorySection/>
             <NumberPadSection>
                <div className="output">
                10000000
