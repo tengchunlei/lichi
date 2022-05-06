a0ab8422a306cb1db3715272997da2acba455835
tengchunlei <5702225+tengchunlei@user.noreply.gitee.com>
2022-05-06 16:06:14 +0800

    NumberPad wip

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -17,27 +17,7 @@ function Money() {
             <TagsSection/>
             <NoteSection/>
             <CategorySection/>
-            <NumberPadSection>
-               <div className="output">
-               10000000
-               </div>
-               <div className="pad clearfix">
-                   <button>1</button>
-                   <button>2</button>
-                   <button>3</button>
-                   <button>删除</button>
-                   <button>4</button>
-                   <button>5</button>
-                   <button>6</button>
-                   <button>清空</button>
-                   <button>7</button>
-                   <button>8</button>
-                   <button>9</button>
-                   <button className="ok">OK</button>
-                   <button className="zero">0</button>
-                   <button className="dot">.</button>
-               </div>
-           </NumberPadSection>
+            <NumberPadSection/>
         </MyLayout>
     );
 }
