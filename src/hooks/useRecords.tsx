eeba158116fe12b6bc8a287e9d705f170855242b
tengchunlei <5702225+tengchunlei@user.noreply.gitee.com>
2022-06-07 14:24:00 +0800

    check input

--- src/hooks/useRecords.tsx
+++ src/hooks/useRecords.tsx
@@ -8,7 +8,6 @@ type RecordItem={
      createAt:string
 }
 type newRecordItem=Omit<RecordItem,'createAt'>
-
  export const useRecords=()=>{
     const [records,setRecords]=useState<RecordItem[]>([]);
     useEffect(()=>{
@@ -16,13 +15,20 @@ type newRecordItem=Omit<RecordItem,'createAt'>
     },[]);
 
     useUpdate(()=>{
-        window.localStorage.setItem('reords',JSON.stringify(records))
+        window.localStorage.setItem('records',JSON.stringify(records))
     },[records])
 
      const addRecord =(newRecord:newRecordItem)=>{
+        if(newRecord.amount<=0){
+            alert('请输入金额');
+            return false;}
+        if(newRecord.tagIds.length===0){
+            alert('请选择标签');
+            return false;
+        }
           const record ={...newRecord,createAt:(new Date()).toISOString()}
          setRecords([...records,record]);
+           return true;
      }
-
     return {records,addRecord}
  };
