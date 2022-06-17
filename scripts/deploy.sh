7b792c4feae57cde29720031ae79affecdec2b9d
tengchunlei <5702225+tengchunlei@user.noreply.gitee.com>
2022-06-17 12:25:21 +0800

    update title

---
+++ scripts/deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+yarn build &&
+cd build  &&
+git init &&
+git add . &&
+git commit -m 'deploy' &&
+git remote add origin git@github.com:tengchunlei/lichi-website.git &&
+git push origin -u origin master -f
+cd -
