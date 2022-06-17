37b3eb4d582ed5fb2b8107bbd6cf2bf0719a5d2f
tengchunlei <5702225+tengchunlei@user.noreply.gitee.com>
2022-06-17 14:07:06 +0800

    add deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+
 yarn build &&
 cd build  &&
 git init &&
 git add . &&
 git commit -m 'deploy' &&
 git remote add origin git@github.com:tengchunlei/lichi-website.git &&
-git push origin -u origin master -f
+git push -u origin master -f
 cd -
